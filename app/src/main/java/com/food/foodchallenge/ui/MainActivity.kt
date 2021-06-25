93a39e163dd0de0163dfe36fa3625befbd9f076b
victor <victor@voxtour.com>
2021-06-25 20:00:06 +0300

    create empty android project

---
+++ app/src/main/java/com/food/foodchallenge/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.food.foodchallenge.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.food.foodchallenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
